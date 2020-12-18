2cccd8478cb2c04d147f6fb86726917e891bedca
Gustavo Marin <gustavo.marin@intelygenz.com>
2020-12-18 15:11:39 +0100

    split kre version test into different files. remove goerr113 for tests files.

---
+++ pkg/cmd/kre/version/version_actions_test.go
@@ -0,0 +1,164 @@
+package version_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/golang/mock/gomock"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+
+	"github.com/konstellation-io/kli/api/kre/version"
+	"github.com/konstellation-io/kli/internal/config"
+	"github.com/konstellation-io/kli/internal/testhelpers"
+	"github.com/konstellation-io/kli/mocks"
+	cmd "github.com/konstellation-io/kli/pkg/cmd/kre/version"
+)
+
+type testVersionSuite struct {
+	ctrl  *gomock.Controller
+	mocks versionSuiteMocks
+}
+
+type versionSuiteMocks struct {
+	kreClient *mocks.MockKreInterface
+	version   *mocks.MockVersionInterface
+}
+
+func newTestVersionSuite(t *testing.T) *testVersionSuite {
+	ctrl := gomock.NewController(t)
+
+	return &testVersionSuite{
+		ctrl,
+		versionSuiteMocks{
+			kreClient: mocks.NewMockKreInterface(ctrl),
+			version:   mocks.NewMockVersionInterface(ctrl),
+		},
+	}
+}
+
+func setupVersionConfig(t *testing.T, f *mocks.MockCmdFactory) {
+	cfg := f.Config()
+
+	err := cfg.AddServer(config.ServerConfig{
+		Name:     "test",
+		URL:      "http://test.local",
+		APIToken: "12346",
+	})
+	require.NoError(t, err)
+	err = cfg.SetDefaultServer("test")
+	require.NoError(t, err)
+}
+
+func TestVersionListCmd(t *testing.T) {
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
+		s.mocks.version.EXPECT().List("runtime1234").Return(version.List{
+			{ID: "1234", Name: "greeter-v1", Status: "STARTED"},
+			{ID: "6578", Name: "greeter-v2", Status: "STOPPED"},
+		}, nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.Run("version ls --runtime runtime1234 ").
+		Contains(heredoc.Doc(`
+        ID   NAME       STATUS
+			1 1234 greeter-v1 STARTED
+			2 6578 greeter-v2 STOPPED
+		`))
+}
+
+func TestVersionStartNoMessageCmd(t *testing.T) {
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.RunE("version start 12345", fmt.Errorf("required flag(s) \"message\" not set"))
+}
+
+func TestVersionStartCmd(t *testing.T) {
+	comment := "test start comment"
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
+		s.mocks.version.EXPECT().Start("12345", comment).Return(nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.Runf("version start 12345 --message \"%s\"", comment).
+		Contains(heredoc.Doc(`
+      [✔] Starting version '12345'.
+		`))
+}
+
+func TestVersionStopCmd(t *testing.T) {
+	comment := "test stop comment"
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
+		s.mocks.version.EXPECT().Stop("12345", comment).Return(nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.Runf("version stop 12345 --message \"%s\"", comment).
+		Contains(heredoc.Doc(`
+      [✔] Stopping version '12345'.
+		`))
+}
+
+func TestVersionPublishCmd(t *testing.T) {
+	comment := "test publish comment"
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
+		s.mocks.version.EXPECT().Publish("12345", comment).Return(nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.Runf("version publish 12345 --message \"%s\"", comment).
+		Contains(heredoc.Doc(`
+      [✔] Publishing version '12345'.
+		`))
+}
+
+func TestVersionUnpublishCmd(t *testing.T) {
+	comment := "test unpublish comment"
+	s := newTestVersionSuite(t)
+	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
+		setupVersionConfig(t, f)
+
+		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
+		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
+		s.mocks.version.EXPECT().Unpublish("12345", comment).Return(nil)
+
+		return cmd.NewVersionCmd(f)
+	})
+
+	r.Runf("version unpublish 12345 --message \"%s\"", comment).
+		Contains(heredoc.Doc(`
+      [✔] Unpublishing version '12345'.
+		`))
+}
--- pkg/cmd/kre/version/version_config_test.go
+++ pkg/cmd/kre/version/version_config_test.go
@@ -1,4 +1,4 @@
-package version_test
+package version
 
 import (
 	"fmt"
@@ -10,13 +10,13 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/konstellation-io/kli/api/kre/version"
+
 	"github.com/konstellation-io/kli/internal/config"
 	"github.com/konstellation-io/kli/internal/testhelpers"
 	"github.com/konstellation-io/kli/mocks"
-	cmd "github.com/konstellation-io/kli/pkg/cmd/kre/version"
 )
 
-type testVersionSuite struct {
+type testVersionConfigSuite struct {
 	ctrl  *gomock.Controller
 	mocks versionSuiteMocks
 }
@@ -26,10 +26,10 @@ type versionSuiteMocks struct {
 	version   *mocks.MockVersionInterface
 }
 
-func newTestVersionSuite(t *testing.T) *testVersionSuite {
+func newTestVersionConfigSuite(t *testing.T) *testVersionConfigSuite {
 	ctrl := gomock.NewController(t)
 
-	return &testVersionSuite{
+	return &testVersionConfigSuite{
 		ctrl,
 		versionSuiteMocks{
 			kreClient: mocks.NewMockKreInterface(ctrl),
@@ -51,107 +51,8 @@ func setupVersionConfig(t *testing.T, f *mocks.MockCmdFactory) {
 	require.NoError(t, err)
 }
 
-func TestVersionListCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
-	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
-		setupVersionConfig(t, f)
-
-		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
-		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
-		s.mocks.version.EXPECT().List("runtime1234").Return(version.List{
-			{ID: "1234", Name: "greeter-v1", Status: "STARTED"},
-			{ID: "6578", Name: "greeter-v2", Status: "STOPPED"},
-		}, nil)
-
-		return cmd.NewVersionCmd(f)
-	})
-
-	r.Run("version ls --runtime runtime1234 ").
-		Contains(heredoc.Doc(`
-        ID   NAME       STATUS
-			1 1234 greeter-v1 STARTED
-			2 6578 greeter-v2 STOPPED
-		`))
-}
-
-func TestVersionStartCmd(t *testing.T) {
-	comment := "test start comment"
-	s := newTestVersionSuite(t)
-	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
-		setupVersionConfig(t, f)
-
-		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
-		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
-		s.mocks.version.EXPECT().Start("12345", comment).Return(nil)
-
-		return cmd.NewVersionCmd(f)
-	})
-
-	r.Runf("version start 12345 --message \"%s\"", comment).
-		Contains(heredoc.Doc(`
-      [✔] Starting version '12345'.
-		`))
-}
-
-func TestVersionStopCmd(t *testing.T) {
-	comment := "test stop comment"
-	s := newTestVersionSuite(t)
-	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
-		setupVersionConfig(t, f)
-
-		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
-		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
-		s.mocks.version.EXPECT().Stop("12345", comment).Return(nil)
-
-		return cmd.NewVersionCmd(f)
-	})
-
-	r.Runf("version stop 12345 --message \"%s\"", comment).
-		Contains(heredoc.Doc(`
-      [✔] Stopping version '12345'.
-		`))
-}
-
-func TestVersionPublishCmd(t *testing.T) {
-	comment := "test publish comment"
-	s := newTestVersionSuite(t)
-	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
-		setupVersionConfig(t, f)
-
-		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
-		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
-		s.mocks.version.EXPECT().Publish("12345", comment).Return(nil)
-
-		return cmd.NewVersionCmd(f)
-	})
-
-	r.Runf("version publish 12345 --message \"%s\"", comment).
-		Contains(heredoc.Doc(`
-      [✔] Publishing version '12345'.
-		`))
-}
-
-func TestVersionUnpublishCmd(t *testing.T) {
-	comment := "test unpublish comment"
-	s := newTestVersionSuite(t)
-	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
-		setupVersionConfig(t, f)
-
-		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
-		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
-		s.mocks.version.EXPECT().Unpublish("12345", comment).Return(nil)
-
-		return cmd.NewVersionCmd(f)
-	})
-
-	r.Runf("version unpublish 12345 --message \"%s\"", comment).
-		Contains(heredoc.Doc(`
-      [✔] Unpublishing version '12345'.
-		`))
-}
-
 func TestVersionGetConfigCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
+	s := newTestVersionConfigSuite(t)
 	versionConfig := &version.Config{
 		Completed: true,
 		Vars: []*version.ConfigVariable{
@@ -174,7 +75,7 @@ func TestVersionGetConfigCmd(t *testing.T) {
 		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
 		s.mocks.version.EXPECT().GetConfig("12345").Return(versionConfig, nil)
 
-		return cmd.NewVersionCmd(f)
+		return NewVersionCmd(f)
 	})
 
 	r.Run("version config 12345").
@@ -188,7 +89,7 @@ func TestVersionGetConfigCmd(t *testing.T) {
 }
 
 func TestVersionGetConfigWithValuesCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
+	s := newTestVersionConfigSuite(t)
 	versionConfig := &version.Config{
 		Completed: true,
 		Vars: []*version.ConfigVariable{
@@ -211,7 +112,7 @@ func TestVersionGetConfigWithValuesCmd(t *testing.T) {
 		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
 		s.mocks.version.EXPECT().GetConfig("12345").Return(versionConfig, nil)
 
-		return cmd.NewVersionCmd(f)
+		return NewVersionCmd(f)
 	})
 
 	r.Run("version config 12345 --show-values").
@@ -225,7 +126,7 @@ func TestVersionGetConfigWithValuesCmd(t *testing.T) {
 }
 
 func TestVersionSetConfigCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
+	s := newTestVersionConfigSuite(t)
 	configVars := []version.ConfigVariableInput{
 		{"key": "key1", "value": "value1"},
 		{"key": "key2", "value": "value2"},
@@ -238,7 +139,7 @@ func TestVersionSetConfigCmd(t *testing.T) {
 		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version)
 		s.mocks.version.EXPECT().UpdateConfig("12345", configVars).Return(true, nil)
 
-		return cmd.NewVersionCmd(f)
+		return NewVersionCmd(f)
 	})
 
 	pair1 := fmt.Sprintf("%s=%s", configVars[0]["key"], configVars[0]["value"])
@@ -250,14 +151,14 @@ func TestVersionSetConfigCmd(t *testing.T) {
 }
 
 func TestVersionSetConfigErrorEdgeCasesCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
+	s := newTestVersionConfigSuite(t)
 
 	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
 		setupVersionConfig(t, f)
 
 		f.EXPECT().KreClient("test").Return(s.mocks.kreClient, nil)
 
-		return cmd.NewVersionCmd(f)
+		return NewVersionCmd(f)
 	})
 
 	type testCase struct {
@@ -281,13 +182,13 @@ func TestVersionSetConfigErrorEdgeCasesCmd(t *testing.T) {
 	}
 
 	for _, pair := range testCases {
-		err := fmt.Errorf(pair.expectedError) // nolint: goerr113
+		err := fmt.Errorf(pair.expectedError)
 		r.RunArgsE("version config 12345", err, pair.extraArgs...)
 	}
 }
 
 func TestVersionSetConfigEdgeCasesCmd(t *testing.T) {
-	s := newTestVersionSuite(t)
+	s := newTestVersionConfigSuite(t)
 
 	r := testhelpers.NewRunner(t, func(f *mocks.MockCmdFactory) *cobra.Command {
 		setupVersionConfig(t, f)
@@ -296,7 +197,7 @@ func TestVersionSetConfigEdgeCasesCmd(t *testing.T) {
 		s.mocks.kreClient.EXPECT().Version().Return(s.mocks.version).AnyTimes()
 		s.mocks.version.EXPECT().UpdateConfig("12345", gomock.Any()).Return(true, nil).AnyTimes()
 
-		return cmd.NewVersionCmd(f)
+		return NewVersionCmd(f)
 	})
 
 	testCases := [][]string{
@@ -304,7 +205,7 @@ func TestVersionSetConfigEdgeCasesCmd(t *testing.T) {
 		{"--set", fmt.Sprintf("%s=%s", "key", "test\n test")},
 		{"--set", fmt.Sprintf("%s=%s", "key", `test`)},
 		{"--set", fmt.Sprintf("%s=%s", "key", "test=")},
-		// This test does not work
+		// This test should work, but it doesn't.
 		// {"--set", fmt.Sprintf("%s=%s", "key", "test \"test")},
 	}
 
